f784673be0b94546c32376cf8307908c3e3bba7c
narapon <43005911+narapons@users.noreply.github.com>
2019-01-18 17:50:19 +0900

    Update MainClass.php

--- src/onebone/itemcloud/MainClass.php
+++ src/onebone/itemcloud/MainClass.php
@@ -313,7 +313,7 @@ class sendItem extends Task{
 	}
 
 	function onRun(int $currentTick){
-		$event = ???->event; //onBreakのイベントを取得したい
+		$event = $this->event;
 		$name = $this->event->getPlayer()->getName();
 		$player = $this->event->getPlayer();
 		if(!$this->event->isCancelled()){
