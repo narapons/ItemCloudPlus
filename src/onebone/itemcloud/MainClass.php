feb71fc41d06e122fba5e58afba4cb5799414063
narapon <43005911+narapons@users.noreply.github.com>
2019-01-19 09:04:20 +0900

    Update MainClass.php

--- src/onebone/itemcloud/MainClass.php
+++ src/onebone/itemcloud/MainClass.php
@@ -317,7 +317,7 @@ class sendItem extends Task{
 		$event = $this->event;
 		$name = $this->event->getPlayer()->getName();
 		$player = $this->event->getPlayer();
-		$onBreak = $this->owner->onBreak;
+		$onBreak = $this->owner->onBreak();
 		if(!$onBreak->isCancelled()){
 			if(!isset($this->owner->clouds[strtolower($name)])){//Mainクラスからやりたい
 				$player->sendMessage("[ItemCloud] ItemCloudのアカウントがありません。作成してください。");
